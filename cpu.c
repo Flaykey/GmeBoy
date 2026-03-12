2050105b4344cd0d7eb4a94b1477b8d63c677711
Flaykey <samratghimire13@gmail.com>
2026-03-13 00:01:03 +0545

    8-bit memory instructions

--- cpu.c
+++ cpu.c
@@ -15,8 +15,157 @@ void LD_A_MEM(CPU *cpu, WORD address)
     cpu->reg.a = cpu->memory[address];
 }
 
-void LD_r8(CPU *cpu, BYTE *dest, BYTE val)
+void LD_r8_n8(CPU *cpu, BYTE *dest, BYTE val)
 {
     *dest = val;
 }
 
+void ADC_A(CPU *cpu, BYTE operand)
+{
+    BYTE carry = GET_FLAG_CARRY(&cpu->reg);
+    WORD sum = cpu->reg.a + operand + carry;
+
+    SET_FLAG_ZERO(&cpu->reg, (sum & 0xFF) == 0);
+    SET_FLAG_SUB(&cpu->reg, 0);
+    SET_FLAG_HALF_CARRY(&cpu->reg, (((cpu->reg.a & 0xF) + (operand & 0xF) + carry) > 0xF));
+    SET_FLAG_CARRY(&cpu->reg, sum > 0xFF);
+
+    cpu->reg.a = sum & 0xFF;
+}
+
+void ADC_A_r8(CPU *cpu, BYTE r)     { ADC_A(cpu, r); }
+void ADC_A_n8(CPU *cpu, BYTE val)   { ADC_A(cpu, val); }
+void ADC_A_MEM(CPU *cpu, WORD addr) { ADC_A(cpu, cpu->memory[addr]); }
+
+void ADD_A(CPU *cpu, BYTE operand)
+{
+
+    WORD sum = cpu->reg.a + operand ;
+
+    SET_FLAG_ZERO(&cpu->reg, (sum & 0xFF) == 0);
+    SET_FLAG_SUB(&cpu->reg, 0);
+    SET_FLAG_HALF_CARRY(&cpu->reg, (((cpu->reg.a & 0xF) + (operand & 0xF)) > 0xF));
+    SET_FLAG_CARRY(&cpu->reg, sum > 0xFF);
+
+    cpu->reg.a = sum & 0xFF;
+}
+
+void ADD_A_r8(CPU *cpu, BYTE r)     { ADD_A(cpu, r); }
+void ADD_A_n8(CPU *cpu, BYTE val)   { ADD_A(cpu, val); }
+void ADD_A_MEM(CPU *cpu, WORD addr) { ADD_A(cpu, cpu->memory[addr]); }
+
+void SBC_A(CPU *cpu, BYTE operand)
+{
+    BYTE carry = GET_FLAG_CARRY(&cpu->reg);
+    WORD sub = cpu->reg.a - operand - carry;
+
+    SET_FLAG_ZERO(&cpu->reg, (sub & 0xFF) == 0);
+    SET_FLAG_SUB(&cpu->reg, 1);
+    SET_FLAG_HALF_CARRY(&cpu->reg, (cpu->reg.a & 0xF) < ((operand & 0xF) + carry));
+    SET_FLAG_CARRY(&cpu->reg, cpu->reg.a < (operand + carry));
+
+    cpu->reg.a = sub & 0xFF;
+}
+
+void SBC_A_r8(CPU *cpu, BYTE r)        {SBC_A(cpu,r);}
+void SBC_A_n8(CPU *cpu, BYTE val)      {SBC_A(cpu,val);}
+void SBC_A_MEM(CPU *cpu, WORD address) {SBC_A(cpu,cpu->memory[address]);}
+
+void SUB_A(CPU *cpu, BYTE operand)
+{
+    WORD sub = cpu->reg.a - operand;
+
+    SET_FLAG_ZERO(&cpu->reg, (sub & 0xFF) == 0);
+    SET_FLAG_SUB(&cpu->reg, 1);
+    SET_FLAG_HALF_CARRY(&cpu->reg, (cpu->reg.a & 0xF) < (operand & 0xF));
+    SET_FLAG_CARRY(&cpu->reg, cpu->reg.a < operand);
+
+    cpu->reg.a = sub & 0xFF;
+}
+
+void SUB_A_r8(CPU *cpu, BYTE r)        {SUB_A(cpu,r);}
+void SUB_A_n8(CPU *cpu, BYTE val)      {SUB_A(cpu,val);}
+void SUB_A_MEM(CPU *cpu, WORD address) {SUB_A(cpu,cpu->memory[address]);}
+
+void INC(CPU *cpu, BYTE *operand)
+{
+    WORD inc = *operand + 1;
+    SET_FLAG_ZERO(&cpu->reg, ((inc & 0xFF) == 0));
+    SET_FLAG_SUB(&cpu->reg,0);
+    SET_FLAG_HALF_CARRY(&cpu->reg, (((*operand & 0xF) + 1) > 0xF));
+
+    *operand = inc & 0XFF;
+}
+void INC_r8(CPU *cpu, BYTE *dest)   {INC(cpu, dest);}
+void INC_MEM(CPU *cpu, WORD address){INC(cpu, &cpu->memory[address]);}
+void INC_SP(CPU *cpu){cpu->reg.SP++;}
+
+void DEC(CPU *cpu, BYTE *operand)
+{
+    WORD dec = *operand - 1;
+
+    SET_FLAG_ZERO(&cpu->reg, ((dec & 0XFF) == 0));
+    SET_FLAG_SUB(&cpu->reg,1);
+    SET_FLAG_HALF_CARRY(&cpu->reg, ((*operand & 0xF) == 0xF));
+
+    *operand = dec & 0XFF;
+}
+
+void DEC_r8(CPU *cpu, BYTE *dest)   {DEC(cpu, dest);}
+void DEC_MEM(CPU *cpu, WORD address){DEC(cpu, &cpu->memory[address]);}
+void DEC_SP(CPU *cpu){cpu->reg.SP--;}
+
+void CP_A_n8(CPU *cpu, BYTE val)
+{
+    WORD res = cpu->reg.a - val;
+    SET_FLAG_ZERO(&cpu->reg,(res & 0xFF) == 0);
+    SET_FLAG_SUB(&cpu->reg,1);
+    SET_FLAG_HALF_CARRY(&cpu->reg, (cpu->reg.a & 0xF) < (val & 0xF));
+    SET_FLAG_CARRY(&cpu->reg, cpu->reg.a < val);
+}
+void CP_A_r8(CPU *cpu, BYTE dest)    { CP_A_n8(cpu,dest);}
+void CP_A_MEM(CPU *cpu, WORD address) { CP_A_n8(cpu, cpu->memory[address]); }
+
+void AND_n8(CPU* cpu, BYTE val)
+{
+    cpu->reg.a &= val;
+    SET_FLAG_ZERO(&cpu->reg, (cpu->reg.a == 0));
+    SET_FLAG_SUB(&cpu->reg, 0);
+    SET_FLAG_HALF_CARRY(&cpu->reg, 1);
+    SET_FLAG_CARRY(&cpu->reg, 0);
+    
+
+}
+void AND_r8(CPU *cpu, BYTE r){AND_n8(cpu,r);}
+void AND_MEM(CPU *cpu, WORD address){AND_n8(cpu,cpu->memory[address]);}
+
+void OR_n8(CPU *cpu, BYTE val)
+{
+    cpu->reg.a |= val;
+    SET_FLAG_ZERO(&cpu->reg, (cpu->reg.a == 0));
+    SET_FLAG_SUB(&cpu->reg, 0);
+    SET_FLAG_HALF_CARRY(&cpu->reg, 0);
+    SET_FLAG_CARRY(&cpu->reg, 0);
+
+}
+void OR_r8(CPU *cpu, BYTE r){OR_n8(cpu,r);}
+void OR_MEM(CPU *cpu, WORD address){OR_n8(cpu,cpu->memory[address]);}
+
+void XOR_n8(CPU *cpu, BYTE val)
+{
+    cpu->reg.a ^= val;
+    SET_FLAG_ZERO(&cpu->reg, (cpu->reg.a == 0));
+    SET_FLAG_SUB(&cpu->reg, 0);
+    SET_FLAG_HALF_CARRY(&cpu->reg, 0);
+    SET_FLAG_CARRY(&cpu->reg, 0);
+
+}
+void XOR_r8(CPU *cpu, BYTE r){XOR_n8(cpu,r);}
+void XOR_MEM(CPU *cpu, WORD address){XOR_n8(cpu,cpu->memory[address]);}
+
+void CPL(CPU *cpu)
+{
+    cpu->reg.a = ~cpu->reg.a;
+    SET_FLAG_SUB(&cpu->reg,1);
+    SET_FLAG_HALF_CARRY(&cpu->reg,1);
+}
--- cpu.h
+++ cpu.h
@@ -23,10 +23,10 @@ typedef struct{
 #define GET_DE(reg) (((WORD)(reg)->d << 8 )| ((reg)->e))
 #define GET_HL(reg) (((WORD)(reg)->h << 8 )| ((reg)->l))
 
-#define SET_FLAG_ZERO(reg,val)          do{ ((reg)->f = (((reg)->f & ~(1 << 7) )|((val & 1) << 7))) } while(0)
-#define SET_FLAG_SUB(reg,val)           do{ ((reg)->f = (((reg)->f & ~(1 << 6) )|((val & 1) << 6))) } while(0)
-#define SET_FLAG_HALF_CARRY(reg,val)    do{ ((reg)->f = (((reg)->f & ~(1 << 5) )|((val & 1) << 5))) } while(0)
-#define SET_FLAG_CARRY(reg,val)         do{ ((reg)->f = (((reg)->f & ~(1 << 4) )|((val & 1) << 4))) } while(0)
+#define SET_FLAG_ZERO(reg,val)          do{ ((reg)->f = (((reg)->f & ~(1 << 7) )|((val & 1) << 7))); } while(0)
+#define SET_FLAG_SUB(reg,val)           do{ ((reg)->f = (((reg)->f & ~(1 << 6) )|((val & 1) << 6))); } while(0)
+#define SET_FLAG_HALF_CARRY(reg,val)    do{ ((reg)->f = (((reg)->f & ~(1 << 5) )|((val & 1) << 5))); } while(0)
+#define SET_FLAG_CARRY(reg,val)         do{ ((reg)->f = (((reg)->f & ~(1 << 4) )|((val & 1) << 4))); } while(0)
 
 #define GET_FLAG_ZERO(reg)        (((reg)->f >> 7) & 1)
 #define GET_FLAG_SUB(reg)         (((reg)->f >> 6) & 1)
@@ -37,15 +37,62 @@ typedef struct{
 void LD_A_r8(CPU *cpu,BYTE r);
 void LD_r8_A(CPU *cpu,BYTE *dest);
 void LD_A_MEM(CPU *cpu, WORD address);
-void LD_r8(CPU *cpu, BYTE *dest, BYTE val);
+void LD_r8_n8(CPU *cpu, BYTE *dest, BYTE val);
 
 //arithmeticinstruction 8-bit
+//addition
+void ADC_A(CPU *cpu, BYTE operand);
+void ADC_A_r8(CPU *cpu, BYTE r);
+void ADC_A_n8(CPU *cpu, BYTE val);
+void ADC_A_MEM(CPU *cpu, WORD address);
+
+void ADD_A(CPU *cpu, BYTE operand);
+void ADD_A_r8(CPU *cpu, BYTE r);
+void ADD_A_n8(CPU *cpu, BYTE val);
+void ADD_A_MEM(CPU *cpu, WORD address);
+
+//subtraction
+void SBC_A(CPU *cpu, BYTE operand);
+void SBC_A_r8(CPU *cpu, BYTE r);
+void SBC_A_n8(CPU *cpu, BYTE val);
+void SBC_A_MEM(CPU *cpu, WORD address);
+
+void SUB_A(CPU *cpu, BYTE operand);
+void SUB_A_r8(CPU *cpu, BYTE r);
+void SUB_A_n8(CPU *cpu, BYTE val);
+void SUB_A_MEM(CPU *cpu, WORD address);
 
 //Increment and decrement instructions
+void INC(CPU* cpu, BYTE *operand);
+void INC_r8(CPU* cpu, BYTE *dest);
+void INC_MEM(CPU* cpu, WORD address);
+void INC_SP(CPU* cpu);
+
+void DEC(CPU* cpu, BYTE *operand);
+void DEC_r8(CPU* cpu, BYTE *dest);
+void DEC_MEM(CPU* cpu, WORD address);
+void DEC_SP(CPU* cpu);
 
 // compare instructions
+void CP_A_n8(CPU* cpu, BYTE dest);
+void CP_A_r8(CPU* cpu, BYTE dest);
+void CP_A_MEM(CPU* cpu, WORD address);
 
 // 8-bit bitwise instructions
+
+void AND_n8(CPU* cpu, BYTE val);
+void AND_r8(CPU* cpu, BYTE r);
+void AND_MEM(CPU* cpu, WORD address);
+
+void OR_n8(CPU* cpu, BYTE val);
+void OR_r8(CPU* cpu, BYTE val);
+void OR_MEM(CPU* cpu, WORD address);
+
+void X0R_n8(CPU* cpu, BYTE val);
+void X0R_r8(CPU* cpu, BYTE val);
+void X0R_MEM(CPU* cpu, WORD address);
+
+void CPL(CPU* cpu);
 
 //control instructions
 
